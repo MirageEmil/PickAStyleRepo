1373bcabafb9c6713ca7adf99c61cf5fc4e79f4f
MirageEmil <118307184+MirageEmil@users.noreply.github.com>
2023-12-01 13:33:29 -0800

    4th
    Player 4 directional movement and no friction on the walls.

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public new Rigidbody2D rb;
+    private Vector2 direction = Vector2.down;
+
+    public KeyCode inputUp = KeyCode.W;
+    public KeyCode inputDown = KeyCode.S;
+    public KeyCode inputLeft = KeyCode.A;
+    public KeyCode inputRight = KeyCode.D;
+
+    public float speed;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(inputUp))
+        {
+            SetDirection(Vector2.up);
+        }
+        else if (Input.GetKey(inputDown))
+        {
+            SetDirection(Vector2.down);
+        }
+        else if (Input.GetKey(inputLeft))
+        {
+            SetDirection(Vector2.left);
+        }
+        else if (Input.GetKey(inputRight))
+        {
+            SetDirection(Vector2.right);
+        }
+        else
+        {
+            SetDirection(Vector2.zero);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+
+        rb.MovePosition(position + translation);
+
+    }
+
+    private void SetDirection(Vector2 newDirection)
+    {
+        direction = newDirection;
+
     }
 }
